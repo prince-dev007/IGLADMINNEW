2466764c3ff152315305892538a157b256132621
asifahmad18 <asifahmad.ahmad61@gmail.com>
2021-04-12 12:24:51 +0530

    removed warnings

--- src/components/pages/Login2.jsx
+++ src/components/pages/Login2.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useHistory,Redirect } from 'react-router';
+import { useHistory } from 'react-router';
 // animation
 import { motion } from "framer-motion";
 // styles
