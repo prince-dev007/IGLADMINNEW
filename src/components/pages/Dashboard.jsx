8528f9943b79145c42e3f98a956c715de3cec727
Asif Ahmad <asifahmad.ahmad61@gmail.com>
2021-11-26 10:37:37 +0530

    dashboard API

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -6,6 +6,8 @@ import { motion } from "framer-motion";
 import Animation from "../../common/Animation";
 // components
 import { Graph1, CardGraph1, CardGraph2, ColumnChart, Donut } from "../partials/Graph";
+import { callAPI } from "../../common/common";
+import { useState } from "react/cjs/react.development";
 
 const Dashboard = () => {
 	useEffect(() => {
@@ -16,39 +18,70 @@ const Dashboard = () => {
 			window.$("#pageSpinner").show();
 		};
 	}, []);
+
+	const [totalAmount, setTotalAmount] = useState(0);
+	const [billCount, setBillCount] = useState(0);
+	const [totalQuantity, setTotalQuantity] = useState(0);
+	const [todayTotalAmt, setTodayTotalAmt] = useState(0);
+	useEffect(() => {
+		const initAPI = async () => {
+			const response = await callAPI({
+				URL: "dashboard/summary",
+			});
+			if (response.status !== 200) return;
+			const data = response.data;
+			setBillCount(data.billCount);
+			setTotalAmount(data.totalAmount);
+			setTotalQuantity(data.totalQuantity);
+			setTodayTotalAmt(data.todayTotalAmt);
+		};
+		initAPI();
+	}, []);
+
 	return (
 		<div className="page-wrapper">
 			<div className="page-content-wrapper">
 				<motion.div initial={Animation.variants.out} animate={Animation.variants.in} exit={Animation.variants.exit} transition={Animation.PageTransition} className="page-content">
 					<div className="row ">
-						<div className="col-sm-12 col-md-4">
+						<div className="col-sm-12 col-md-3">
 							<div className="card " style={{ borderRadius: "15px" }}>
 								<Link style={{ textDecoration: "none" }} to="/sales">
 									<div className="card-body">
 										<div>
-											<CardGraph1 title={"Sales"} count={"23,452"} />
+											<CardGraph1 title={"Sales"} count={"Rs. " + totalAmount} />
 										</div>
 									</div>
 								</Link>
 							</div>
 						</div>
-						<div className="col-sm-12 col-md-4">
+						<div className="col-sm-12 col-md-3">
 							<div className="card " style={{ borderRadius: "15px" }}>
 								<Link style={{ textDecoration: "none" }} to="/sales">
 									<div className="card-body">
 										<div>
-											<CardGraph1 title={"ltrs"} count={"1,344"} />
+											<CardGraph2 title={"Today Sales"} count={"Rs. " + todayTotalAmt} />
 										</div>
 									</div>
 								</Link>
 							</div>
 						</div>
-						<div className="col-sm-12 col-md-4">
+						<div className="col-sm-12 col-md-3">
 							<div className="card " style={{ borderRadius: "15px" }}>
 								<Link style={{ textDecoration: "none" }} to="/sales">
 									<div className="card-body">
 										<div>
-											<CardGraph2 title={"Bills"} count={"454"} />
+											<CardGraph1 title={"Kgs"} count={totalQuantity} />
+										</div>
+									</div>
+								</Link>
+							</div>
+						</div>
+						<div className="col-sm-12 col-md-3">
+							<div className="card " style={{ borderRadius: "15px" }}>
+								<Link style={{ textDecoration: "none" }} to="/sales">
+									<div className="card-body">
+										<div>
+											<CardGraph2 title={"Bills"} count={billCount} />
 										</div>
 									</div>
 								</Link>
